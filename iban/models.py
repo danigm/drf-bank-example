a4cb73f5d968d22c250e0b95c8616ce28bd88ca5
Daniel García Moreno <dani@danigm.net>
2019-08-15 07:26:31 +0200

    iban: Create iban app and IBAN model
    The IBAN number will be related with a user and for the IBAN number
    validation we use the django-localflavor package:
    https://github.com/django/django-localflavor

---
+++ iban/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from localflavor.generic.models import IBANField
+from localflavor.generic.countries.sepa import IBAN_SEPA_COUNTRIES
+
+
+class IBAN(models.Model):
+    user = models.ForeignKey(User, related_name='ibans', on_delete=models.CASCADE)
+    number = IBANField(include_countries=IBAN_SEPA_COUNTRIES)
+
+    def __str__(self):
+        return self.number
